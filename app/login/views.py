9f8c87f13147feb5f46b8abc2a09970514b41b78
venatur <caldos69@gmail.com>
2021-08-18 22:29:09 -0700

    +add more fields on user model +empty values findings
    @todo: create User model instance for saving data

--- app/login/views.py
+++ app/login/views.py
@@ -1,17 +1,45 @@
 from . import login
 from flask import render_template, request, jsonify
+from app.models import User
 
 
 @login.route('/signup')
 def home():
     print("signup")
-    return "hola cliente"
+    return {'hello', 'world'}
 
 
-@login.route('/add', methods=['POST'])
+@login.route('/add', methods=['GET', 'POST'])
 def add():
+    error = None
+    user_list = []
+
+    # get data from JSON
     body = request.get_json()
-    if body is None:
-        return print("no data"), 400
+
+    # if data contains something
+    if body != error:
+        # Verification of POST method
+        if request.method == 'POST':
+            # blucle for empty values findings
+            for item in body:
+                if body.get(item) is not "":
+                    pass
+                else:
+                    return jsonify(
+                        message='missing value in 1 or more parameters',
+                        status=400
+                    )
+
+            return jsonify(
+                message="everything is good",
+                status=200
+            )
+
+        else:
+            return jsonify(message="no es post")
     else:
-        return "ok", 200
+        return jsonify(
+            message="not content",
+            status=400
+        )
--- app/models.py
+++ app/models.py
@@ -1,8 +1,12 @@
 from . import db
 
+
 class User(db.Model):
 
     __tablename__ = "users"
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(32), index=True)
+    name = db.Column(db.String(32))
+    last_name = db.Column(db.String(32))
+    company = db.Column(db.String(32))
     password_hash = db.Column(db.String(128))
